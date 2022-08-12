da79042bfc5ef1d43e0215c65dc9c62b2c6d7179
Ryany0 <109833217+Ryany0@users.noreply.github.com>
2022-08-11 20:23:23 -0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -91,6 +91,60 @@ def startListener(s):
             else:
                 claimedArr = pickle.loads(receiveData)
                 print(claimedArr)
+        receive = s.recv(BUFFER).decode('utf-8')
+        print("recieve msg: ", receive)
+        arg = receive.split(' ')
+        if (arg[0] == "DISCONNECT" or arg[0] == "STOP"):
+            # Stop listener thread
+            print("Press enter again to stop...")
+            LISTENING = False
+            break
+        elif (arg[0] == "LOCK"):
+            # Server tells client that square at (x,y) is locked
+            # LOCK x y
+            x = arg[1]
+            y = arg[2]
+            color = arg[3]
+            # ...code here for client to lock square at (x,y)
+            # ...call functions in Client_GUI.py to manipulate GUI
+            if color != COLOR:
+                GAME_WINDOW.lockPlayersBox(x, y, color)
+        elif (arg[0] == "UNLOCK"):
+            # Server tells client that square at (x,y) is unlocked
+            # UNLOCK x y
+            x = arg[1]
+            y = arg[2]
+            color = arg[3]
+            # ...code here for client to unlock square at (x,y)
+            # ...call functions in Client_GUI.py to manipulate GUI
+            if color != COLOR:
+                GAME_WINDOW.unlockPlayersBox(x, y)
+        elif (arg[0] == "CLAIM"):
+            # Server tells client that square at (x,y) is claimed by (color)
+            # CLAIM x y color
+            x = arg[1]
+            y = arg[2]
+            color = arg[3]
+            GAME_WINDOW.fillBox(x, y, color)
+            # ...code here for client to lock square at (x,y) and color it
+            # ...call functions in Client_GUI.py to manipulate GUI
+        elif (arg[0] == "START"):
+            # Server tells client that game has started
+            claimedStr = arg[1]
+            claimedArr = pickle.loads(claimedStr.encode('utf-8'))
+            print(claimedArr)
+            # ...call functions in Client_GUI.py to manipulate GUI
+        elif (arg[0] == "RESTART"):
+            # Server tells client to restart game (reset GUI)
+            fillerFunc()
+            # ...call functions in Client_GUI.py to manipulate GUI
+        elif (arg[0] == "END"):
+            # Server tells client that game has ended and which color won the game
+            # END color
+            color = arg[1]
+            # ...call functions in Client_GUI.py to manipulate GUI
+        else:
+            print(receive)
 
     s.close()
 
